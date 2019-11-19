f78c0dac35f4c3eef2502f6251137f71db7f55ee
YouMustStudy <rlarmsdn321@naver.com>
2019-11-19 22:04:06 +0900

    Convert To Vector2d and Add Some Resources

--- client/SimpleGame/Object.cpp
+++ client/SimpleGame/Object.cpp
@@ -18,15 +18,76 @@ void Object::InitPhysics()
 {
 	m_type = TYPE_NORMAL;
 
-	m_posX = 0.0f; m_posY = 0.0f; m_posZ = 0.0f;
-	
-	m_velX = 0.0f; m_velY = 0.0f; m_velZ = 0.0f;
-	m_accX = 0.0f; m_accY = 0.0f; m_accZ = 0.0f;
-	m_volX = 0.0f; m_volY = 0.0f; m_volZ = 0.0f;
+	m_pos = Vector2d(0, 0);
+	m_vel = Vector2d(0, 0);
+	m_acc = Vector2d(0, 0);
+	m_vol = Vector2d(0, 0);
 
-	m_r = 1; m_g = 1; m_b = 1; m_a = 1;
+	m_color[0] = 1; m_color[1] = 1; m_color[2] = 1; m_color[3] = 1;
+
 	m_mass = 1.0f;
 	m_friction = 0.0f;
+}
+
+void Object::SetPos(Vector2d pos)
+{
+	m_pos = pos;
+}
+
+void Object::SetPos(float x, float y)
+{
+	m_pos.x = x;
+	m_pos.y = y;
+}
+
+void Object::SetVol(Vector2d vol)
+{
+	m_vol = vol;
+}
+
+void Object::SetColor(float r, float g, float b, float a)
+{
+	m_color[0] = r;
+	m_color[1] = g;
+	m_color[2] = b;
+	m_color[3] = a;
+}
+
+void Object::SetVol(float x, float y)
+{
+	m_vol.x = x;
+	m_vol.y = y;
+}
+
+void Object::SetVel(Vector2d vel)
+{
+	m_vel = vel;
+}
+
+void Object::SetVel(float x, float y)
+{
+	m_vel.x = x;
+	m_vel.y = y;
+}
+
+void Object::SetMass(float mass)
+{
+	m_mass = mass;
+}
+
+void Object::SetFriction(float frict)
+{
+	m_friction = frict;
+}
+
+void Object::SetType(int type)
+{
+	m_type = type;
+}
+
+void Object::SetTex(int tex)
+{
+	m_texID = tex;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////
@@ -36,159 +97,43 @@ void Object::Update(float fTimeElapsed)
 	m_remainingBulletCoolTime -= fTimeElapsed;
 
 	// Apply Friction
-	float velSize = sqrtf(m_velX*m_velX + m_velY * m_velY);
+	float velSize = m_vel.length();
 	if (velSize > 0.0f)
 	{
-		float vX = m_velX / velSize;
-		float vY = m_velY / velSize;
+		Vector2d acc = m_vel;
+		acc.normalize();
 		//Calc friction size
 		float nForce = m_mass * GRAVITY;
 		float frictionSize = nForce * m_friction;
-		float fX = -vX * frictionSize;
-		float fY = -vY * frictionSize;
-		//Calc acc from friction
-		float accX = fX / m_mass;
-		float accY = fY / m_mass;
 
-		float newVelX = m_velX + accX * fTimeElapsed;
-		float newVelY = m_velY + accY * fTimeElapsed;
-		if (newVelX * m_velX < 0.f)
+		acc *= -frictionSize;
+		acc /= m_mass;
+		Vector2d newVel = m_vel + acc * fTimeElapsed;
+
+		if (newVel.x * m_vel.x < 0.f)
 		{
-			m_velX = 0;
+			m_vel.x = 0;
 		}
 		else
 		{
-			m_velX = newVelX;
+			m_vel.x = newVel.x;
 		}
-		if (newVelY * m_velY < 0.f)
+		if (newVel.y * m_vel.y < 0.f)
 		{
-			m_velY = 0;
+			m_vel.y = 0;
 		}
 		else
 		{
-			m_velY = newVelY;
+			m_vel.y = newVel.y;
 		}
 	}
 
-	m_posX += m_velX * fTimeElapsed;
-	m_posY += m_velY * fTimeElapsed;
-	m_posZ += m_velZ * fTimeElapsed;
+	m_pos += m_vel * fTimeElapsed;
 }
-
-
-void Object::GetType(int *type)
+void Object::AddForce(Vector2d force, float fElapsedTime)
 {
-	*type = this->m_type;
-}
-void Object::SetType(int type)
-{
-	this->m_type = type;
-}
-void Object::GetTex(int *tex)
-{
-	*tex = this->m_texID;
-}
-void Object::SetTex(int tex)
-{
-	this->m_texID = tex;
-}
-void Object::GetPos(float *x, float *y, float *z) 
-{
-	*x = this->m_posX;
-	*y = this->m_posY;
-	*z = this->m_posZ;
-}
-void Object::SetPos(float x, float y, float z)
-{
-	this->m_posX = x;
-	this->m_posY = y;
-	this->m_posZ = z;
-}
-
-void Object::GetMass(float *mass)
-{
-	*mass = this->m_mass;
-}
-void Object::SetMass(float mass)
-{
-	this->m_mass = mass;
-}
-
-void Object::GetFriction(float *friction)
-{
-	*friction = this->m_friction;
-}
-void Object::SetFriction(float friction)
-{
-	this->m_friction = friction;
-}
-
-void Object::GetAcc(float *x, float *y, float *z)
-{
-	*x = this->m_accX;
-	*y = this->m_accY;
-	*z = this->m_accZ;
-}
-void Object::SetAcc(float x, float y, float z)
-{
-	this->m_accX = x;
-	this->m_accY = y;
-	this->m_accZ = z;
-}
-
-void Object::GetVel(float *x, float *y, float *z)
-{
-	*x = this->m_velX;
-	*y = this->m_velY;
-	*z = this->m_velZ;
-}
-void Object::SetVel(float x, float y, float z)
-{
-	this->m_velX = x;
-	this->m_velY = y;
-	this->m_velZ = z;
-}
-
-void Object::GetVol(float *x, float *y, float *z)
-{
-	*x = this->m_volX;
-	*y = this->m_volY;
-	*z = this->m_volZ;
-}
-void Object::SetVol(float x, float y, float z)
-{
-	this->m_volX = x;
-	this->m_volY = y;
-	this->m_volZ = z;
-}
-
-void Object::GetColor(float *r, float *g, float *b, float *a)
-{
-	*r = this->m_r;
-	*g = this->m_g;
-	*b = this->m_b;
-	*a = this->m_a;
-}
-void Object::SetColor(float r, float g, float b, float a) 
-{
-	this->m_r = r;
-	this->m_g = g;
-	this->m_b = b;
-	this->m_a = a;
-}
-
-void Object::AddForce(float x, float y, float z, float fElapsedTime)
-{
-	float accX, accY, accZ;
-	accX = accY = accZ = 0;
-
-	accX = x / m_mass;
-	accY = y / m_mass;
-	accZ = z / m_mass;
-
-	m_velX = m_velX + accX * fElapsedTime;
-	m_velY = m_velY + accY * fElapsedTime;
-	m_velZ = m_velZ + accZ * fElapsedTime;
+	Vector2d acc = force / m_mass;
+	m_vel += acc * fElapsedTime;
 }
 
 bool Object::CanShootBullet()
