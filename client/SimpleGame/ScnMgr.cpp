c22dce22840484c9b9f14ef3f4dc612ade14b58c
YouMustStudy <rlarmsdn321@naver.com>
2019-11-23 03:05:36 +0900

    이동 동기화

--- client/SimpleGame/ScnMgr.cpp
+++ client/SimpleGame/ScnMgr.cpp
@@ -1,12 +1,16 @@
+#pragma once
+
 #include "stdafx.h"
 #include "ScnMgr.h"
 #include "Dependencies\freeglut.h"
-
+#include "..\\..\\server\server\PacketMgr.h"
 #include <WinSock2.h>
 
-ScnMgr::ScnMgr(int socket)
+ScnMgr::ScnMgr(int socket, int id)
 {
+	//NW
 	soc = socket;
+	MYID = id;
 
 	m_Renderer = new Renderer(m_Width, m_Height);
 	if (!m_Renderer->IsInitialized())
@@ -66,14 +70,14 @@ ScnMgr::ScnMgr(int socket)
 	m_item[3]->m_visible = true;
 
 	//Add Hero Object
-	m_players[HERO_ID] = Player();
-	m_players[HERO_ID].SetPos(0.0f, 0.0f);
-	m_players[HERO_ID].SetColor(1.0f, 1.0f, 1.0f, 1.0f);
-	m_players[HERO_ID].SetVol(0.3f, 0.3f);
-	m_players[HERO_ID].SetVel(0.0f, 0.0f);
-	m_players[HERO_ID].SetMass(1.0f);
-	m_players[HERO_ID].SetFriction(0.6f);
-	m_players[HERO_ID].SetTex(textures[1]);
+	m_players[MYID] = Player();
+	m_players[MYID].SetPos(0.0f, 0.0f);
+	m_players[MYID].SetColor(1.0f, 1.0f, 1.0f, 1.0f);
+	m_players[MYID].SetVol(0.3f, 0.3f);
+	m_players[MYID].SetVel(0.0f, 0.0f);
+	m_players[MYID].SetMass(1.0f);
+	m_players[MYID].SetFriction(0.6f);
+	m_players[MYID].SetTex(textures[1]);
 
 	//Add Test Object
 	m_players[1] = Player();
@@ -95,85 +99,85 @@ ScnMgr::~ScnMgr()
 
 void ScnMgr::Update(float fTimeElapsed)
 {
-	//Move Player
-	Vector2d keyDir(0, 0);
-	if (m_keyW)
-	{
-		keyDir.y += 1.0f;
-	}
-	if (m_keyS)
-	{
-		keyDir.y -= 1.0f;
-	}
-	if (m_keyA)
-	{
-		keyDir.x -= 1.0f;
-	}
-	if (m_keyD)
-	{
-		keyDir.x += 1.0f;
-	}
-	if (keyDir.length() > 0.0000001f)
-	{
-		float fAmount = 10.0f;
-		keyDir.normalize();
-		keyDir *= fAmount;
-		m_players[HERO_ID].AddForce(keyDir, fTimeElapsed);
-	}
+	////Move Player
+	//Vector2d keyDir(0, 0);
+	//if (m_keyW)
+	//{
+	//	keyDir.y += 1.0f;
+	//}
+	//if (m_keyS)
+	//{
+	//	keyDir.y -= 1.0f;
+	//}
+	//if (m_keyA)
+	//{
+	//	keyDir.x -= 1.0f;
+	//}
+	//if (m_keyD)
+	//{
+	//	keyDir.x += 1.0f;
+	//}
+	//if (keyDir.length() > 0.0000001f)
+	//{
+	//	float fAmount = 10.0f;
+	//	keyDir.normalize();
+	//	keyDir *= fAmount;
+	//	m_players[HERO_ID].AddForce(keyDir, fTimeElapsed);
+	//}
 
-	//Shoot Bullet
-	if (m_mouseLeft == true && m_players[HERO_ID].CanShootBullet()) {
-		m_players[HERO_ID].ShootBullet(m_mousepos);
-	}
+	////Shoot Bullet
+	//if (m_mouseLeft == true && m_players[HERO_ID].CanShootBullet()) {
+	//	m_players[HERO_ID].ShootBullet(m_mousepos);
+	//}
 
-	for (auto& p_player : m_players)
-	{
-		p_player.second.Update(fTimeElapsed);
-	}
+	//for (auto& p_player : m_players)
+	//{
+	//	p_player.second.Update(fTimeElapsed);
+	//}
 
-	//Collision Detect
-	for (auto& p_pair : m_players)
-	{
-		if (p_pair.second.m_visible == false) continue;
-		auto& player = p_pair.second;
+	////Collision Detect
+	//for (auto& p_pair : m_players)
+	//{
+	//	if (p_pair.second.m_visible == false) continue;
+	//	auto& player = p_pair.second;
 
-		//플레이어, 벽 체크
-		for (auto& wall : m_wall)
-		{
-			if (player.isOverlap(*wall)) {
-				player.correctpos(*wall);
-			}
-		}
+	//	//플레이어, 벽 체크
+	//	for (auto& wall : m_wall)
+	//	{
+	//		if (player.isOverlap(*wall)) {
+	//			player.correctpos(*wall);
+	//		}
+	//	}
 
-		//플레이어, 아이템 체크
-		for (auto& item : m_item)
-		{
-			if (item->m_visible == false) continue;
-			if (player.isOverlap(*item)) {
-				player.weapon = item->type;
-				item->m_visible = false;
-			}
-		}
+	//	//플레이어, 아이템 체크
+	//	for (auto& item : m_item)
+	//	{
+	//		if (item->m_visible == false) continue;
+	//		if (player.isOverlap(*item)) {
+	//			player.weapon = item->type;
+	//			item->m_visible = false;
+	//		}
+	//	}
 
-		//총알, 플레이어 체크
-		for (auto & bullet : player.bullets)
-		{
-			if (bullet.m_visible == false) continue;
-			for (auto& p_other : m_players)
-			{
-				if (p_other.second.m_visible == false) continue;
-				if (p_other.first == p_pair.first) continue;
-				if (p_other.second.isOverlap(bullet)) {
-					bullet.m_visible = false;
-					//데미지처리 & 체력이 0이하면
-					if (p_other.second.getDamage(bullet))
-						p_other.second.die();
-				}
-			}
-		}
-	}
+	//	//총알, 플레이어 체크
+	//	for (auto & bullet : player.bullets)
+	//	{
+	//		if (bullet.m_visible == false) continue;
+	//		for (auto& p_other : m_players)
+	//		{
+	//			if (p_other.second.m_visible == false) continue;
+	//			if (p_other.first == p_pair.first) continue;
+	//			if (p_other.second.isOverlap(bullet)) {
+	//				bullet.m_visible = false;
+	//				//데미지처리 & 체력이 0이하면
+	//				if (p_other.second.getDamage(bullet))
+	//					p_other.second.die();
+	//			}
+	//		}
+	//	}
+	//}
 
-	DoGarbageCollection();
+	//DoGarbageCollection();
 }
 void ScnMgr::RenderScene()
 {
@@ -202,41 +206,45 @@ void ScnMgr::RenderScene()
 
 void ScnMgr::KeyDownInput(unsigned char key, int x, int y)
 {
+	int packet = 0;
 	if (key == 'w' || key == 'W')
 	{
-		m_keyW = true;
+		packet = make_packet_input(MYID, input_Wdown);
 	}
 	else if (key == 'a' || key == 'A')
 	{
-		m_keyA = true;
+		packet = make_packet_input(MYID, input_Adown);
 	}
 	else if (key == 's' || key == 'S')
 	{
-		m_keyS = true;
+		packet = make_packet_input(MYID, input_Sdown);
 	}
 	else if (key == 'd' || key == 'D')
 	{
-		m_keyD = true;
+		packet = make_packet_input(MYID, input_Ddown);
 	}
+	send(soc, (const char*)& packet, sizeof(int), 0);
 }
 void ScnMgr::KeyUpInput(unsigned char key, int x, int y)
 {
+	int packet = 0;
 	if (key == 'w' || key == 'W')
 	{
-		m_keyW = false;
+		packet = make_packet_input(MYID, input_Wup);
 	}
 	else if (key == 'a' || key == 'A')
 	{
-		m_keyA = false;
+		packet = make_packet_input(MYID, input_Aup);
 	}
 	else if (key == 's' || key == 'S')
 	{
-		m_keyS = false;
+		packet = make_packet_input(MYID, input_Sup);
 	}
 	else if (key == 'd' || key == 'D')
 	{
-		m_keyD = false;
+		packet = make_packet_input(MYID, input_Dup);
 	}
+	send(soc, (const char*)& packet, sizeof(int), 0);
 }
 void ScnMgr::SpecialKeyDownInput(unsigned char key, int x, int y)
 {
